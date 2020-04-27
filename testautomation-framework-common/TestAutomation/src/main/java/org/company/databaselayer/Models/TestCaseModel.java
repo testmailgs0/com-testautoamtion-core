b93d382846a0ddfd3c59d82c99281731f6049eab
testmailgs0 <30926716+testmailgs0@users.noreply.github.com>
2020-04-27 22:21:36 +0530

    hibernate.cfg.xml

--- testautomation-framework-common/TestAutomation/src/main/java/org/company/databaselayer/Models/TestCaseModel.java
+++
@@ -1,5 +0,0 @@
-package org.company.databaselayer.Models;
-
-public class TestCaseModel {
-
-}
--- testautomation-framework-common/TestAutomation/src/main/java/org/company/testexecutor/TestExecutor.java
+++ testautomation-framework-common/TestAutomation/src/main/java/org/company/testexecutor/TestExecutor.java
@@ -13,7 +13,10 @@ import java.util.List;
 import java.util.Map;
 
 import org.company.bussinesslayer.Controls.Login;
+//import org.company.databaselayer.TempDBTest;
+import org.company.databaselayer.Configurations.DBConfinguration;
 import org.company.utilities.TestNGHelper;
+import org.hibernate.SessionFactory;
 import org.testng.TestNG;
 import org.testng.xml.XmlClass;
 import org.testng.xml.XmlInclude;
@@ -24,21 +27,21 @@ import org.testng.xml.XmlTest;
 import org.yaml.snakeyaml.reader.UnicodeReader;
 
 public class TestExecutor extends TestNGHelper {
-	public static void main(String[] args)
-	{
-		List<String> SuiteNames = Arrays.asList("Regression,Smoke".split(","));
-		List<String> TNames = Arrays.asList("Claims,Contracts".split(","));
-		List<String> Tclasses = Arrays.asList("Login,ServiceDate".split(","));
-		setSuiteDetails(SuiteNames,ParallelMode.FALSE);
-		HashMap<String,List<String>> TestToAdd = new HashMap<String,List<String>>();
-		TestToAdd.put("Regression", TNames);
-		setTestDetails(TestToAdd);
-		HashMap<String,List<String>> ClassesToAdd = new HashMap<String,List<String>>();
-		ClassesToAdd.put("Claims",Tclasses);
-		//ClassesToAdd.put("Contracts",Tclasses);
-		setClassDetails(ClassesToAdd);
-		generateTestNGFiles();
-		runtTestNgFiles();
+	@SuppressWarnings("static-access")
+	public static void main(String[] args) {
+		DBConfinguration obj = new DBConfinguration();
+		obj.LoadConfiguration();
+		/*
+		 * List<String> SuiteNames = Arrays.asList("Regression,Smoke".split(","));
+		 * List<String> TNames = Arrays.asList("Claims,Contracts".split(","));
+		 * List<String> Tclasses = Arrays.asList("Login,ServiceDate".split(","));
+		 * setSuiteDetails(SuiteNames,ParallelMode.FALSE); HashMap<String,List<String>>
+		 * TestToAdd = new HashMap<String,List<String>>(); TestToAdd.put("Regression",
+		 * TNames); setTestDetails(TestToAdd); HashMap<String,List<String>> ClassesToAdd
+		 * = new HashMap<String,List<String>>(); ClassesToAdd.put("Claims",Tclasses);
+		 * //ClassesToAdd.put("Contracts",Tclasses); setClassDetails(ClassesToAdd);
+		 * generateTestNGFiles(); runtTestNgFiles();
+		 */
 	}
 
 }
