39776d119692d65973f756ec2fd89772f3236e35
testmailgs0 <30926716+testmailgs0@users.noreply.github.com>
2020-04-28 18:37:16 +0530

    hibernate-new-configuration-3.5 --After Execution Commit

--- testautomation-framework-common/TestAutomation/src/main/java/org/company/testexecutor/TestExecutor.java
+++ testautomation-framework-common/TestAutomation/src/main/java/org/company/testexecutor/TestExecutor.java
@@ -1,27 +1,33 @@
 package org.company.testexecutor;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 import org.company.databaselayer.Configurations.DBConfinguration;
 import org.company.utilities.TestNGHelper;
+import org.testng.xml.XmlSuite.ParallelMode;
 
 public class TestExecutor extends TestNGHelper {
 	@SuppressWarnings("static-access")
 	public static void main(String[] args) {
 
-		DBConfinguration obj2 = new DBConfinguration();
-		obj2.LoadConfiguration();
-
 		/*
-		 * List<String> SuiteNames = Arrays.asList("Regression,Smoke".split(","));
-		 * List<String> TNames = Arrays.asList("Claims,Contracts".split(","));
-		 * List<String> Tclasses = Arrays.asList("Login,ServiceDate".split(","));
-		 * setSuiteDetails(SuiteNames,ParallelMode.FALSE);
-		 * HashMap<String,List<String>>TestToAdd = new HashMap<String,List<String>>();
-		 * TestToAdd.put("Regression",TNames); setTestDetails(TestToAdd);
-		 * HashMap<String,List<String>> ClassesToAdd= new
-		 * HashMap<String,List<String>>(); ClassesToAdd.put("Claims",Tclasses);
-		 * ClassesToAdd.put("Contracts",Tclasses); setClassDetails(ClassesToAdd);
-		 * generateTestNGFiles(); runtTestNgFiles();
+		 * DBConfinguration obj2 = new DBConfinguration(); obj2.LoadConfiguration();
 		 */
+
+		
+		  List<String> SuiteNames = Arrays.asList("Regression,Smoke".split(","));
+		  List<String> TNames = Arrays.asList("Claims,Contracts".split(","));
+		  List<String> Tclasses = Arrays.asList("Login,ServiceDate".split(","));
+		  setSuiteDetails(SuiteNames,ParallelMode.FALSE);
+		  HashMap<String,List<String>>TestToAdd = new HashMap<String,List<String>>();
+		  TestToAdd.put("Regression",TNames); setTestDetails(TestToAdd);
+		  HashMap<String,List<String>> ClassesToAdd= new
+		  HashMap<String,List<String>>(); ClassesToAdd.put("Claims",Tclasses);
+		  ClassesToAdd.put("Contracts",Tclasses); setClassDetails(ClassesToAdd);
+		  generateTestNGFiles(); runtTestNgFiles();
+		 
 
 	}
 
